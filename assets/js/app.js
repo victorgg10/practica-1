2a2f5e9c9374a51814e70bb879254450e7d575b6
Victor <garciavictorm997@gmail.com>
2023-02-03 15:39:39 -0800

    se agregaron comentarios a cambios realizados

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+//se cambio la variable ya que se estan llamando por clase
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+//se agrego asyn ya que es necesario para una funcion asincrona
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //se modifico el consol log
+  console.log(response);
+  // se cambio de commillas simples a `
+  $n.textContent = `${response.name}`;
+  $b.textContent = `${response.blog}`;
+  $l.textContent = `${response.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  //se modifico nombre de la variable n a $n
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
